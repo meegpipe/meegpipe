3f20310e00469ce218cbe81aabee8a606d3538a6
German Gomez-Herrero <german.gomezherrero@gmail.com>
2013-10-11 09:55:32 +0200

    Bring repo germangh/matlab_io

---
+++ +io/+mff2/read_header.m
@@ -0,0 +1,23 @@
+function hdr = read_header(filename)
+
+import mperl.split;
+
+% A minimalistic header
+hdr.begin_time = perl('+io/+mff2/private/record_time.pl', filename);
+hdr.signal = signal_info(filename);
+
+epochInfo = perl('+io/+mff2/private/parse_epochs.pl', filename);
+epochInfo = split(char(10), epochInfo);
+
+for j = 1:numel(epochInfo)
+    val = cellfun(@(x) str2double(x), split(';', epochInfo{j}));
+    thisEpoch.begin_time = val(1);
+    thisEpoch.end_time = val(2);
+    thisEpoch.first_block = val(3);
+    thisEpoch.last_block = val(4);
+    hdr.epochs(j) = thisEpoch;    
+end
+
+
+
+end
