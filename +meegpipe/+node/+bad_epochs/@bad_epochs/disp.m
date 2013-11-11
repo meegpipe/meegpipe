d800adc413ae332fdb015a26b960de6d83a22fb2
German Gomez-Herrero <german.gomezherrero@kasku.org>
2013-11-11 19:10:10 +0100

    bad_epochs.disp is consistent with bad_channels.disp

--- +meegpipe/+node/+bad_epochs/@bad_epochs/disp.m
+++ +meegpipe/+node/+bad_epochs/@bad_epochs/disp.m
@@ -7,14 +7,27 @@ disp_class_info(obj);
 
 disp_body(obj);
 
-disp_body(get_config(obj));
+cfg = get_config_handle(obj);
 
-crit = get_config(get_config(obj, 'Criterion'));
-if ~isempty(crit),
-    fprintf('\nNode criterion properties:\n\n');
-    disp_body(crit);
+if ~isempty(cfg) && numel(fieldnames(cfg)) > 0,
+    fprintf('\nNode configuration options:\n\n');
+    disp_body(cfg);
 end
 
+crit = get_config(obj, 'Criterion');
+
+if ~isempty(crit),
+    fprintf('\nNode criterion properties:\n\n');
+    
+    if isa(crit, 'goo.configurable'),
+        critCfg = get_config(crit);
+        disp_body(critCfg);
+    else
+        warning('off', 'MATLAB:structOnObject');
+        disp(struct(crit));
+        warning('on', 'MATLAB:structOnObject');
+    end
+end
 
 fprintf('\n');
 
