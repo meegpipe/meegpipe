2830424052663c1bbef4526b141425dc894d76b5
German Gomez-Herrero <german.gomezherrero@gmail.com>
2013-12-02 16:13:32 +0100

    More restrictive location for user-defined meegpipe.ini

--- +meegpipe/get_config.m
+++ +meegpipe/get_config.m
@@ -4,19 +4,20 @@ function value = get_config(varargin)
 import mperl.config.inifiles.inifile;
 import meegpipe.root_path;
 import mperl.file.spec.catfile;
+import mperl.file.spec.rel2abs;
+
 
 sysIni  = catfile(root_path, 'meegpipe.ini');
 userIni = catfile(root_path, '..', '..', 'meegpipe.ini');
-
-if ~exist(userIni, 'file'), 
-    userIni = catfile(root_path, '..', '..', '..', 'meegpipe.ini');
-end
 
 if ~exist(userIni, 'file'),
     userIni = catfile(pwd, 'meegpipe.ini');
 end
 
 if exist(userIni, 'file'),
+    userIni = rel2abs(userIni);
+    warning('get_config:UserDefinedConfig', ...
+        'Using user-defined configuration: %s', userIni);
     cfg = inifile(userIni);
 elseif exist(sysIni, 'file')    
     cfg = inifile(sysIni);
