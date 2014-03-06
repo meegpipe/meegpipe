2efbd6e2bbcc45b6ac4b298c31731ed82c60a1a9
German Gomez-Herrero <g@germangh.com>
2014-03-06 22:15:22 +0100

    Quick and dirty fix for broken report FID

--- +report/+generic/@generic/get_fid.m
+++ +report/+generic/@generic/get_fid.m
@@ -1,5 +1,15 @@
 function fid = get_fid(obj)
 
+import mperl.file.spec.catfile;
+import safefid.safefid;
+
 fid = obj.FID;
+
+if isa(fid, 'safefid.safefid') && ~fid.Valid,
+    % A quick and dirty fix. This should never happen, but it does due to a
+    % bug hidden somewhere
+    obj.FID = safefid.fopen(catfile(obj.RootPath, obj.FileName), 'a+');
+    fid = obj.FID;
+end
 
 end
