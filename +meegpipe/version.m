68d832a9077f71f58d09d45fc1bcffb2bae91a96
German Gomez-Herrero <german.gomezherrero@gmail.com>
2014-03-06 10:51:29 +0100

    More reobust meegpipe.version

--- +meegpipe/version.m
+++ +meegpipe/version.m
@@ -3,14 +3,33 @@ function id = version()
 import mperl.file.spec.rel2abs;
 import safefid.safefid;
 
+FILE_NAME = '.git/refs/heads/master';
+
 dirName = rel2abs([meegpipe.root_path filesep '..']);
 
 currDir = pwd;
 try
     cd(dirName);
-    fid = safefid.fopen('.git/refs/heads/master', 'r');
-    id = fid.fgetl;
-
+    if exist(FILE_NAME, 'file')
+        fid = safefid.fopen(FILE_NAME, 'r');
+        id = fid.fgetl;
+    else
+        % If the user followed the installation instructions on the web,
+        % then his meegpipe installation dir is named
+        % meegpipe/meegpipe-meegpipe-[version]
+        dirName = rel2abs('.');
+        match = regexp(dirName, 'meegpipe-meegpipe-(?<id>\w+)$', 'names');
+        if isempty(match)
+            warning('meegpipe:version:Unknown', ...
+                ['Could not figure out meegpipe version. You may have ' ... 
+                'problems reproducing your results in the future. ' ...
+                'To solve this issue, follow exactly the installation ' ...
+                'instructions at http://germangh.com/meegpipe']);
+            id = 'unknown';
+        else
+            id = match.id;
+        end        
+    end
 catch ME
     cd(currDir);
     rethrow(ME);
