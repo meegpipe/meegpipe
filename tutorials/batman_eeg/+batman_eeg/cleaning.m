08ec3837c215bee9281ba850b260e69fa9783b5e
German Gomez-Herrero <german.gomezherrero@gmail.com>
2014-03-14 12:35:37 +0100

    batman_eeg: extracting simple spectral features

--- tutorials/batman_eeg/+batman_eeg/cleaning.m
+++ tutorials/batman_eeg/+batman_eeg/cleaning.m
@@ -6,43 +6,34 @@ import mperl.file.find.finddepth_regex_match;
 import misc.process_arguments;
 import misc.split_arguments;
 
-opt.Test     = false;
 opt.Date     = datestr(now, 'yymmdd_HHMMSS');
-opt.Subjects = 1:10; 
-opt.Conditions = {'arsq', 'baseline', 'pvt', 'rs'};
+opt.Subject  = 1:10; 
+opt.Condition = {'arsq', 'baseline', 'pvt', 'rs'};
+opt.OutputDir = '';
+
 [thisArgs, varargin] = split_arguments(opt, varargin);
 [~, opt] = process_arguments(opt, thisArgs, [], true);
+
+if isempty(opt.OutputDir),
+    opt.OutputDir = ['/data1/projects/batman/analysis/cleaning/'  opt.Data];
+end
 
 % Just in case you forgot to do it when you started MATLAB
 meegpipe.initialize;
 
-% The directory where the cleaning results should be stored
-if opt.Test,
-   ROOT_DIR = '/data1/projects/batman/analysis/cleaning/tests_IGNORE_THIS/';  
-else
-   ROOT_DIR = '/data1/projects/batman/analysis/cleaning/'; 
-end
-OUTPUT_DIR = [ROOT_DIR opt.Date];
-
 % Ensure the directory exists (Unix-specific)
-system(['mkdir -p ' OUTPUT_DIR]);
-
-PARALELLIZE = true; % Should each file be processed in parallel?
-DO_REPORT   = true; % Should full HTML reports be generated?
-
+system(['mkdir -p ' opt.OutputDir]);
 
 myPipe = batman_eeg.cleaning_pipeline(...
-    'GenerateReport', DO_REPORT, ...
-    'Parallelize',    PARALELLIZE, ...
     varargin{:});
 
 somsds.link2rec(...
     'batman',       ...                  % The recording ID
-    'subject',      opt.Subjects, ...    % The subject ID(s)
+    'subject',      opt.Subject, ...     % The subject ID(s)
     'modality',     'eeg', ...           % The data modality
-    'condition',    opt.Conditions, ...
-    'file_regex',   '\.pset', ...     % Only pset/pseth files
-    'folder',       OUTPUT_DIR);
+    'condition',    opt.Condition, ...
+    'file_regex',   '\.pset', ...        % Only pset/pseth files
+    'folder',       opt.OutputDir);
 
 regex = 'rs_\d+\.pseth$';
 files = finddepth_regex_match(OUTPUT_DIR, regex);
---
+++ tutorials/batman_eeg/+batman_eeg/spectral_analysis_pipeline.m
@@ -0,0 +1,65 @@
+function myPipe = spectral_analysis_pipeline(varargin)
+
+import meegpipe.node.*;
+
+nodeList = {};
+
+%% Node 1: Data import
+myImporter = physioset.import.physioset;
+myNode = physioset_import.new('Importer', myImporter);
+nodeList = [ nodeList {myNode}];
+
+%% Node 2: Copy the data
+nodeList = [ nodeList {copy.new('Path', @() tempdir)} ];
+
+%% Node 5: reject bad channels (again!)
+myCrit = bad_channels.criterion.var.new(...
+    'Max', @(x) median(x) + 1.5*mad(x), 'MaxCard', 5);
+myNode = bad_channels.new('Criterion', myCrit);
+nodeList = [nodeList {myNode}];
+
+%% Node 3: channel interpolation
+myNode = chan_interp.new('NN', 4);
+nodeList = [nodeList {myNode}];
+
+%% Node 4: Spectral analysis
+% The bands of interest.
+% Features and topographies will be plotted for these bands only.
+myROI = mjava.hash;
+myROI('theta') = {[4 8], [4 40]};
+myROI('alpha') = {[8 12], [4 40]};
+myROI('beta1') = {[12 20], [4 40]};
+myROI('beta2') = {[20 40], [4 40]};
+
+% Plot PSDs only between 4 and 40 Hz
+plotterPSD = @(sr) plotter.psd.psd(...
+    'FrequencyRange', [3 40], ...
+    'BOI',            myROI, ...     
+    'LogData',        false);
+
+% Do not compute spectral features from peripheral channels (probably more
+% affected by noise). 
+sens  = sensors.eeg.from_template('egi256');
+xyz  = cartesian_coords(sens); 
+z = xyz(:,3);
+z = z - min(z);
+z = z./max(z);
+isPeripheral = z < 0.4;
+channels = labels(subset(sens, ~isPeripheral));
+
+myNode = spectra.new(...
+    'ROI',          myROI, ...
+    'PlotterPSD',   plotterPSD, ...
+    'Channels',     channels);
+nodeList = [nodeList {myNode}];
+
+%% Create the pipeline
+myPipe = pipeline.new(...
+    'Name',             'spectral_analysis', ...
+    'NodeList',         nodeList, ...
+    'Save',             false, ...
+    varargin{:});
+
+
+
+end
