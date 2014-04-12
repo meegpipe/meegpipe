24c8ad7ed59945b4d95d836c9d7c7a4ddae5d63f
German Gomez-Herrero <german.gomezherrero@kasku.org>
2014-04-12 12:46:28 +0200

    FIX: has_inkscape

--- +inkscape/has_inkscape.m
+++ +inkscape/has_inkscape.m
@@ -6,8 +6,8 @@ function bool = has_inkscape()
 % Where
 %
 % BOOL is true if Inkscape is installed in this sytem, and meegpipe is able
-% to find it (i.e. the path to the Inkscape binary is in specified in the 
-% meegpipe configuration file meegpipe.ini). 
+% to find it (i.e. the path to the Inkscape binary is in specified in the
+% meegpipe configuration file meegpipe.ini).
 %
 %
 % See also: inkscape
@@ -15,7 +15,7 @@ function bool = has_inkscape()
 cmd = sprintf('"%s" -V', inkscape.inkscape_binary);
 [~, res] = system(cmd);
 
-match = regexp(res, 'Inkscape\s+\d+\.\d+\.\d+', 'once');
+match = regexp(res, 'Inkscape\s+\d+\.\d+', 'once');
 
 if isempty(match),
     bool = false;
