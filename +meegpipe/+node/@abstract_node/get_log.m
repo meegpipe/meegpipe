278d9e5f0eeaaa2773b7c080dccd6a6c4a26c5b6
German Gomez-Herrero <german.gomezherrero@kasku.org>
2014-04-17 14:09:59 +0200

    FIX: more robust get_log for abstract_node

--- +meegpipe/+node/@abstract_node/get_log.m
+++ +meegpipe/+node/@abstract_node/get_log.m
@@ -18,9 +18,12 @@ if isempty(fidIdx),
     obj.LogMap_(filename) = fidIdx;
     
 else
-    
-    fid = obj.LogFID_{fidIdx};
-
+    if numel(obj.LogFID_) >= fidIdx && ~isempty(obj.LogFID_{fidIdx}),
+        fid = obj.LogFID_{fidIdx};
+    else
+        fid = safefid.fopen(filename, 'a');
+        obj.LogFID_{fidIdx} = fid;
+    end
 end
 
 end
