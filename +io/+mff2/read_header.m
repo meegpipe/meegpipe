9b6d03810f5bde2b69c262279ae7e1dca5f037c6
German Gomez-Herrero <g@germangh.com>
2014-05-06 10:47:37 +0200

    mff importer can handle missing epochs.xml files

--- +io/+mff2/read_header.m
+++ +io/+mff2/read_header.m
@@ -1,10 +1,20 @@
 function hdr = read_header(filename)
 
 import mperl.split;
+import mperl.file.spec.catfile;
 
 % A minimalistic header
 hdr.begin_time = perl('+io/+mff2/private/record_time.pl', filename);
 hdr.signal = signal_info(filename);
+
+epochsFilename = catfile(filename, 'epochs.xml');
+
+if ~exist(epochsFilename, 'file'),
+    warning('mff2:MissingEpochsInfo', ...
+        'File epochs.xml is missing from %s', filename);   
+    hdr.epochs = [];
+    return;
+end
 
 epochInfo = perl('+io/+mff2/private/parse_epochs.pl', filename);
 epochInfo = split(char(10), epochInfo);
