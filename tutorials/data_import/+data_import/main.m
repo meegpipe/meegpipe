5dac3800d297b775739fe34a575615d9c8f54539
German Gomez-Herrero <g@germangh.com>
2014-05-17 22:02:00 +0200

    Update data_import tutorial

--- tutorials/data_import/+data_import/main.m
+++ tutorials/data_import/+data_import/main.m
@@ -3,10 +3,11 @@ myImporter = physioset.import.mff('Precision', 'single');
 
 % Build a node that nodes how to generate a physioset from an .mff file
 myNode = meegpipe.node.physioset_import.new(...
-    'Importer', myImporter, ...  % Read data from an .mff file
-    'Save',     true, ...        % Save the node output (as a .pset/pseth)
-    'Name',     'mff2pset', ...  % Optional, just to have nice dir names
-    'OGE',      true ...        % If possible, use Open Grid Engine
+    'Importer', myImporter, ... % Read data from an .mff file
+    'Save',     true, ...       % Save the node output (as a .pset/pseth)
+    'OGE',      true, ...       % Use Open Grid Engine, if available
+    'Queue',    'short.q@somerenserver.herseninstituut.knaw.nl', ...
+    'Name',     'mff2pset' ...  % Optional, just to have nice dir names
 );
 
 % Create symbolic links to all sleep .mff files in the ssmd recording
