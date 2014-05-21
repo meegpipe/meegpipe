58399eb9fb44588fa5f6e83646bc89c708ac3b81
German Gomez-Herrero <german.gomezherrero@kasku.org>
2014-05-21 09:43:58 +0200

    Modify klaus tutorial pipeline

--- tutorials/klaus/+tutorial_klaus/create_pipeline.m
+++ tutorials/klaus/+tutorial_klaus/create_pipeline.m
@@ -8,13 +8,20 @@ myNode = meegpipe.node.physioset_import.new(...
 nodeList = [nodeList, {myNode}];
 
 myNode = meegpipe.node.filter.new(...
-    'Filter', @(sr) filter.hpfilt('fc', 0.5/(sr/2)));
+    'Filter', @(sr) filter.hpfilt('fc', 0.2/(sr/2)));
 nodeList = [nodeList, {myNode}];
 
-myNode = meegpipe.node.bad_channels.new;
+% myNode = meegpipe.node.bad_channels.new;
+% nodeList = [nodeList, {myNode}];
+myRejCrit = meegpipe.node.bad_channels.criterion.var.new(...
+    'Max', @(x) median(x) + mad(x));
+myNode = meegpipe.node.bad_channels.new('Criterion', myRejCrit);
 nodeList = [nodeList, {myNode}];
 
 myNode = meegpipe.node.bad_epochs.sliding_window(1, 2);
+nodeList = [nodeList, {myNode}];
+
+myNode = meegpipe.node.smoother.new;
 nodeList = [nodeList, {myNode}];
 
 myNode = aar.pwl.new('IOReport', report.plotter.io);
