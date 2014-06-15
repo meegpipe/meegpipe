4de33290c9b5e8894c9f93ee10afbf3dd6d573ce
German Gomez-Herrero <german.gomezherrero@kasku.org>
2014-06-15 11:32:19 +0200

    Trigger an error if trying to filter a too short signal

--- +filter/@ba/filter.m
+++ +filter/@ba/filter.m
@@ -15,17 +15,23 @@ if verbose,
     clear +misc/eta;
 end
 
+delay = ceil((numel(obj.B) - 1)/2);
+
+if 5*delay > size(x, 2),
+    error(['Signal length (%d) must be at least 5 times as long as the ' ...
+        'filter (%d)'], size(x,2), delay);
+end
+
 for i = 1:size(x,1),
     
     y = filter(obj.B, obj.A, x(i, :));
     if numel(obj.A) > 1,
         x(i,:) = y;
-    else
-        delay = ceil((numel(obj.B) - 1)/2);
-        x(i, 1:end-delay) = y((delay+1):end); 
+    else    
+        x(i, 1:end-delay) = y((delay+1):end);
         x(i, end-delay+1:end) = fliplr(x(i, end-2*delay+1:end-delay));
     end
-   
+    
     if verbose,
         eta(tinit, size(x,1), i, 'remaintime', false);
     end
