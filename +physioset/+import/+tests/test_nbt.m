0418e0801a16ff5f14676fc1c8202563b1cbb937
German Gomez-Herrero <german.gomezherrero@kasku.org>
2014-06-15 16:35:37 +0200

    NBT importer passes tests

--- +physioset/+import/+tests/test_nbt.m
+++ +physioset/+import/+tests/test_nbt.m
@@ -13,12 +13,12 @@ import misc.set_warning_status;
 
 % The sample data file to be used for testing
 DATA_FILE = 'NBT.S0021.090205.EOR1';
-DATA_URL = 'http://kasku.org/data/meegpipe/';
+DATA_URL = meegpipe.get_config('test', 'remote');
 WARN_IDS = { 'sensors:InvalidLabel', 'sensors:MissingPhysDim' };
 
 MEh     = [];
 
-initialize(6);
+initialize(5);
 
 %% Create a new session
 try
@@ -64,10 +64,10 @@ try
         urlwrite([DATA_URL DATA_FILE '_info.mat'], [DATA_FILE '_info.mat']);
     end   
     stat = set_warning_status(WARN_IDS, 'off');
-    data = import(physioset.import.nbt, [DATA_FILE '.mat']);
+    data = import(physioset.import.nbt, [DATA_FILE '_info.mat']);
     set_warning_status(WARN_IDS, stat);
     
-    ok(all(size(data) == [3 6000]) & numel(get_event(data)) == 6, name);
+    ok(all(size(data) == [129 59577]) & numel(get_event(data)) == 1, name);
     
     clear data;
     
@@ -82,62 +82,24 @@ catch ME
     
 end
 
-%% import another sample data
-try
-    
-    name = 'import another sample data file';
-    
-    folder = session.instance.Folder;
-    
-    [fPath, fName, fExt] = fileparts(DATA_FILE_2);
-    evFile = [fPath fName '.events.csv'];
-    if ~exist(DATA_FILE_2, 'file'),
-        urlwrite([DATA_URL fName fExt], DATA_FILE_2);
-        urlwrite([DATA_URL fName '.events.csv'], evFile);
-    end
-    files = somsds.link2files({DATA_FILE_2, evFile}, folder);
-    stat = set_warning_status(WARN_IDS, 'off');
-    data = import(poly5, files{1});
-    set_warning_status(WARN_IDS, stat);
-    
-    ok(all(size(data) == [37 1743921]) & numel(get_event(data)) == 0, name);
-    
-    clear data;
-    
-    
-catch ME
-    
-    set_warning_status(WARN_IDS, 'on');
-    clear data;
-    ok(ME, name);
-    MEh = [MEh ME];
-    
-end
 
 %% specify file name
 try
     
     name = 'specify file name';
     
-    folder = session.instance.Folder;
-    
-    [fPath, fName, fExt] = fileparts(DATA_FILE);
-    evFile = [fPath fName '.events.csv'];
-    if ~exist(DATA_FILE, 'file'),
-        urlwrite([DATA_URL fName fExt], DATA_FILE);
-        urlwrite([DATA_URL fName '.events.csv'], evFile);
+    if ~exist([DATA_FILE '.mat'], 'file'),
+        urlwrite([DATA_URL DATA_FILE '.mat'], [DATA_FILE '.mat']);
+        urlwrite([DATA_URL DATA_FILE '_info.mat'], [DATA_FILE '_info.mat']);
     end
-    files = somsds.link2files({DATA_FILE, evFile}, folder);
-    myImporter = poly5(...
-        'FileName',     catfile(folder, 'myfile'), ...
-        'Temporary',    false);
     stat = set_warning_status(WARN_IDS, 'off');
-    import(myImporter, files{1});
-    set_warning_status(WARN_IDS, stat);
     
-    psetExt = pset.globals.get.DataFileExt;
-    newFile = catfile(folder, ['myfile' psetExt]);
-    ok(exist(newFile, 'file') > 0, name);
+    newFile = catfile(session.instance.Folder, 'myfile.pset');
+    data = import(physioset.import.nbt('FileName', newFile), ...
+        [DATA_FILE '_info.mat']);
+    set_warning_status(WARN_IDS, stat);
+
+    ok(all(size(data) == [129 59577]) && exist(newFile, 'file') > 0, name);
     
 catch ME
     
--- +physioset/+import/@nbt/import.m
+++ +physioset/+import/@nbt/import.m
@@ -7,6 +7,7 @@ function pObj = import(obj, varargin)
 import physioset.physioset;
 import pset.file_naming_policy;
 import pset.globals;
+import mperl.file.spec.catfile;
 
 if numel(varargin) == 1 && iscell(varargin{1}),
     varargin = varargin{1};
@@ -40,14 +41,41 @@ else
     newFileName = obj.FileName;
 end
 
+[path, name, ext] = fileparts(fileName);
+if isempty(regexp(name, '_info$', 'once')),
+    % user provided the data file, not the info file
+    infoFileName = catfile(path, [name '_info' ext]);
+    dataFileName = fileName;
+else
+    dataFileName = catfile(path, [name(1:end-5) ext]);
+    infoFileName = fileName;
+end
 
-cmd = sprintf('EEG = nbt_NBTsignal2EEGlab(''%s'');',fileName);
-evalc(cmd);
-pObj = physioset.from_nbt(EEG, EEG.NBTinfo, ...
-    'FileName', newFileName, 'SensorClass', obj.SensorClass);
+nbtData = load(dataFileName);
+fNames = fieldnames(nbtData);
+if numel(fNames) > 1,
+    error('Invalid format for file %s', dataFileName);
+end
+nbtData = nbtData.(fNames{1});
 
+nbtInfo = load(infoFileName);
+fNames = fieldnames(nbtInfo);
+if numel(fNames) > 1,
+    error('Invalid format for file %s', infoFileName);
+end
+nbtInfo = nbtInfo.(fNames{1});
 
-%% Undoing stuff 
+if verbose,
+    fprintf([verboseLabel 'Importing NBT file %s into %s ...\n\n'], ...
+        infoFileName, newFileName);
+end
+pObj = physioset.physioset.from_nbt(nbtInfo, nbtData, ...
+    'Filename', newFileName, 'SensorClass', obj.SensorClass);
+
+if ~isempty(obj.Sensors),
+    % Sensors property takes precedence over SensorClass
+    set_sensors(pObj, obj.Sensors);
+end
 
 % Unset the global verbose
 goo.globals.set('VerboseLabel', origVerboseLabel);
