d4cb5c71c451916ecd82d874ffd9af090eb3ba22
German Gomez-Herrero <german.gomezherrero@kasku.org>
2014-06-17 09:16:46 +0200

    Static hash code of nodes do not depend on -static.ini

--- +meegpipe/+node/@abstract_node/get_static_hash_code.m
+++ +meegpipe/+node/@abstract_node/get_static_hash_code.m
@@ -7,9 +7,10 @@ tmpHash('Config')       = get_hash_code(get_config(obj));
 tmpHash('DataSelector') = struct(obj.DataSelector);
 
 
-if ~isempty(obj.Static_),
-    tmpHash('Static_')  = get_hash_code(obj.Static_);
-end
+% An experiment ...
+%if ~isempty(obj.Static_),
+%    tmpHash('Static_')  = get_hash_code(obj.Static_);
+%end
 
 % We do not take into account Parent_ and NodeIndex_. The hash code should
 % identify only local node changes.
