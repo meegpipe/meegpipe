b76a4e593214df3e7ed21b8a6865e37dc20661cc
German Gomez-Herrero <german.gomezherrero@kasku.org>
2014-06-17 09:37:36 +0200

    Roll back previous commit

--- +meegpipe/+node/@abstract_node/get_static_hash_code.m
+++ +meegpipe/+node/@abstract_node/get_static_hash_code.m
@@ -7,10 +7,9 @@ tmpHash('Config')       = get_hash_code(get_config(obj));
 tmpHash('DataSelector') = struct(obj.DataSelector);
 
 
-% An experiment ...
-%if ~isempty(obj.Static_),
-%    tmpHash('Static_')  = get_hash_code(obj.Static_);
-%end
+if ~isempty(obj.Static_),
+    tmpHash('Static_')  = get_hash_code(obj.Static_);
+end
 
 % We do not take into account Parent_ and NodeIndex_. The hash code should
 % identify only local node changes.
