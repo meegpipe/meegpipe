40a6d2db360f18921e71cc4207ee4d01aaa2c65d
German Gomez-Herrero <german.gomezherrero@kasku.org>
2015-02-13 10:50:49 +0100

    BUG: Removing temporary files too early
    This bug will most likely break meegpipe when running through OGE.
    You should update to the current version asap.

--- +meegpipe/+node/@abstract_node/run_oge.m
+++ +meegpipe/+node/@abstract_node/run_oge.m
@@ -107,15 +107,6 @@ else
     cmd5 = sprintf('delete(''%s'');', dataFile);
 end
 
-%% Cleanup all the temp dirs within the .meegpipe directories
-cmd6 = '';
-cleanup_dirs = {};
-if isa(obj, 'meegpipe.node.node'),
-    tempdir = get_tempdir(obj);
-    cmd6 = ['system(''rm -rf ' tempdir ''')'];
-    cleanup_dirs = [cleanup_dirs; {tempdir}];
-end
-
 %% Call qsub or condor_q
 
 cmd00 = sprintf('close all; clear all; clear classes;');
@@ -133,7 +124,7 @@ if has_condor && strcmp(obj.Queue, 'condor'),
         'Log',       log, ...
         'MatlabLog', matlabLog);
 else
-    qsub([cmd00 cmd01 cmd0 cmd1 cmd2 cmd3 cmd4 cmd5 cmd6], ...
+    qsub([cmd00 cmd01 cmd0 cmd1 cmd2 cmd3 cmd4 cmd5], ...
         'Name',         jobName, ...
         'Queue',        get_queue(obj), ...
         'CleanupDirs',  cleanup_dirs);
